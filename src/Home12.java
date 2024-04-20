bf1807c9e9b794b3f884a09c2e84244f9cce2423
Pandrik <166299398+Pandrik@users.noreply.github.com>
2024-04-20 11:09:40 +0500

    Комит 3 Правильное решение

--- src/Home12.java
+++ src/Home12.java
@@ -3,28 +3,20 @@ import java.util.Scanner;
 public class Home12 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int min;
-        int ch1 = console.nextInt();    //считываю первое число
-        int ch2 = console.nextInt();    //считываю второе число
-        if (ch1 > ch2) {                //определяю большее и переопределяю переменные как ch1<ch2
-            min = ch2;
-            ch2 = ch1;
-            ch1 = min;
-        }
-        while (console.hasNextInt()) {      //вхожу в цикл если введено число
-            int ch3 = console.nextInt();
-            if (ch3 < ch1 && ch3 < ch2) {   // список всех возможных вариантов с переназначением ch1 и ch2
-                ch2 = ch1;
-                ch1 = ch3;
-            } else if (ch3 > ch1 && ch3 < ch2) {
-                ch2 = ch3;
-            } else if (ch3 > ch1 && ch3 > ch2 && ch1 == ch2) {
-                ch2 = ch3;
-            } else if (ch1 == ch2 & ch3 > ch1) {
-                ch2 = ch3;
+
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        while (console.hasNextInt()) {
+            int tmp = console.nextInt();
+            if (tmp < min) {
+                secondMin = min;
+                min = tmp;
+            } else if (tmp > min && tmp < secondMin) {
+                secondMin = tmp;
             }
         }
-        System.out.println(ch2);
+        System.out.println(secondMin);
     }
 }
 
