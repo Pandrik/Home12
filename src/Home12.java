c3555b74e020e54ab4391a15e8b4089f9930c5b4
Pandrik <166299398+Pandrik@users.noreply.github.com>
2024-04-20 11:04:26 +0500

    Комит 2

--- src/Home12.java
+++ src/Home12.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 public class Home12 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int min2 = 0;
         int min;
-        int ch1 = console.nextInt();
-        int ch2 = console.nextInt();
-        if (ch1 > ch2) {
+        int ch1 = console.nextInt();    //считываю первое число
+        int ch2 = console.nextInt();    //считываю второе число
+        if (ch1 > ch2) {                //определяю большее и переопределяю переменные как ch1<ch2
             min = ch2;
             ch2 = ch1;
             ch1 = min;
         }
-        min2 = ch2;
-        while (console.hasNextInt()) {
+        while (console.hasNextInt()) {      //вхожу в цикл если введено число
             int ch3 = console.nextInt();
-            if (ch3 < ch1 && ch3 < ch2) {
-                min2 = ch1;
+            if (ch3 < ch1 && ch3 < ch2) {   // список всех возможных вариантов с переназначением ch1 и ch2
+                ch2 = ch1;
+                ch1 = ch3;
             } else if (ch3 > ch1 && ch3 < ch2) {
-                min2 = ch3;
-            } else if (ch3 > ch1 && ch3 > ch2 && ch1 != ch2) {
-                min2 = ch2;
+                ch2 = ch3;
+            } else if (ch3 > ch1 && ch3 > ch2 && ch1 == ch2) {
+                ch2 = ch3;
             } else if (ch1 == ch2 & ch3 > ch1) {
-                min2 = ch3;
-            } else if (ch3 < ch1 && ch3 == ch2) {
-                min2 = ch1;
+                ch2 = ch3;
             }
         }
-        System.out.println(min2);
+        System.out.println(ch2);
     }
 }
 
